94a6b1c98f93ff7d7b224e4af8e2d2a6ddb4fbcd
Lorenzo Marcolli <lory.marcolli@gmail.com>
2023-06-17 16:30:07 +0200

    ci: add fake shiping cost

--- lambda/GetShippingCost.js
+++ lambda/GetShippingCost.js
@@ -2,6 +2,25 @@ export const handler = async(event) => {
 
     let response;
 
+    // check if shippingAddress is empty
+    if (!event.shippingAddress) {
+        response = {
+            statusCode: 400,
+            body: JSON.stringify({ "message": "empty_shipping_address" }),
+        }
+        return response
+    }
+
+    // make some operaton to create fake shipping cost
+    let shippingAddress = event.shippingAddress
+    let shippingCost = 0
+    if (shippingAddress.country === "Brazil") {
+        shippingCost = 10
+    }
+    else {
+        shippingCost = 20
+    }
+
     // TODO implement
     response = {
         statusCode: 200,

d1525c2b844db46836dcf390d9c11393958700dc
Alberto Rizzi <albertorizzi@MBP-di-Alberto.homenet.telecomitalia.it>
2023-06-20 19:24:39 +0200

    feat: add api and websocket

--- client/src/components/section/CartRecap.tsx
+++ client/src/components/section/CartRecap.tsx
@@ -3,25 +3,25 @@
 import { Checkout, Product } from '@/types/generic'
 import { getCartItemCount, getCartItems, getCartTotal } from '@/utils/cart'
 import React from 'react'
-import {useRouter} from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 type Props = {}
 
-function CartRecap({}: Props) {
+function CartRecap({ }: Props) {
 
     const router = useRouter()
 
     const [loading, setLoading] = React.useState(false)
 
 
-    const placeOrder = async() => {
+    const placeOrder = async () => {
 
 
 
         setLoading(true)
 
         const order: Checkout = {
-            basket: getCartItems().map((item:Product) => ({
+            basket: getCartItems().map((item: Product) => ({
                 id: item.id,
                 quantity: item.quantity,
             })),
@@ -40,29 +40,34 @@ function CartRecap({}: Props) {
 
 
         console.log('place order')
+
+        const res = await fetch('https://gogmb2rqs6.execute-api.eu-central-1.amazonaws.com/default/PurchaseProduct',
+            { method: "POST", body: JSON.stringify(order) })
+
+        const data: any = await res.json()
+
+        console.log(data)
+
         setTimeout(() => {
             setLoading(false)
-                    // redirect to order page
-        router.push(`/order?callbackUrl=${encodeURIComponent('wss://socketsbay.com/wss/v2/1/demo/')}`)
-
-        }
-        , 2000)
-
+            // redirect to order page
+            router.push(`/order?callbackUrl=${encodeURIComponent(data.callbackUrl + '?orderId=' + data.orderId)}`)
+        }, 2000)
     }
 
-  return (
-    <div>
+    return (
+        <div>
             <h2>Recap</h2>
             <section className='my-4 bg-slate-200 p-4 rounded-md'>
                 <p>Subtotal: {getCartTotal()}</p>
                 <p>Quantity: {getCartItemCount()}</p>
             </section>
             <button
-            onClick={placeOrder}
-            disabled={loading}
-            className=
-            
-            ' disabled:opacity-50 disabled:animate-pulse bg-blue-500 w-full relative hover:bg-blue-700 text-white font-bold py-4 px-8 '>
+                onClick={() => placeOrder()}
+                disabled={loading}
+                className=
+
+                'disabled:opacity-50 disabled:animate-pulse bg-blue-500 w-full relative hover:bg-blue-700 text-white font-bold py-4 px-8 '>
                 {
                     loading ? (
                         <>Loading...</>
@@ -72,11 +77,11 @@ function CartRecap({}: Props) {
 
                 }
             </button>
-            
-       </div>
-  )
+
+        </div>
+    )
 }
 
-  
+
 
 export default CartRecap

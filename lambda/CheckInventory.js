8ca50cf843b0c5da508bfae773fcc51b9d202534
Alberto Rizzi <albertorizzi@MBP-di-Alberto.homenet.telecomitalia.it>
2023-06-20 20:44:55 +0200

    add lambda

--- lambda/CheckInventory.js
+++ lambda/CheckInventory.js
@@ -1,33 +1,57 @@
-const db = [
-    { "id": 1, "availability": 10, "price": 10 },
-    { "id": 2, "availability": 2, "price": 5 },
-    { "id": 3, "availability": 5, "price": 3 }
-];
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
+
+const dynamoDBClient = new DynamoDBClient();
 
 export const handler = async (event, context, callback) => {
+    let response;
+    const basket = event.basket;
 
     try {
-        let response;
+        const hasInvalidQuantity = await Promise.all(
+            basket.map(async (product) => {
+                console.log(typeof product.id);
 
-        // check if basket is empty
-        if (!event.basket) {
-            throw new Error("empty_basket")
-        }
+                const params = {
+                    TableName: 'Inventory',
+                    Key: {
+                        'productId': { N: product.id.toString() }
+                    }
+                };
+                
+                console.log(params)
 
-        const basket = event.basket;
-        const hasInvalidQuantity = basket.some((product) => {
-            const availability = db.find(o => o.id === product.id).availability;
-            if (product.quantity > availability) {
-                return true; // Found an invalid quantity, exit the loop
-            }
-            return false;
-        });
+                const command = new GetItemCommand(params);
 
-        if (hasInvalidQuantity) {
-            throw new Error("error_quantity")
-        }
+                console.log(command);
+
+                const data = await dynamoDBClient.send(command);
+
+                console.log(data);
+
+                // Check if the item exists
+                if (!data.Item) {
+                    throw new Error(`Product with ID ${product.id} not found in the inventory.`);
+                }
+
+                const retrievedItem = data.Item;
+
+                console.log(retrievedItem);
+
+                const availability = parseInt(retrievedItem.quantity.N);
+
+                if (product.quantity > availability) {
+                    return true; // Found an invalid quantity, exit the loop
+                }
+
+                return false;
+            })
+        );
         
-        else {
+        console.log(hasInvalidQuantity)
+
+        if (hasInvalidQuantity.some(Boolean)) {
+            throw new Error("error_quantity");
+        } else {
             response = {
                 statusCode: 200,
                 body: event,
@@ -35,14 +59,13 @@ export const handler = async (event, context, callback) => {
         }
 
         return response;
-    }
-    catch (error) {
+    } catch (error) {
         const myErrorObj = {
             message: error.message,
             errorType: "InternalServerError",
             httpStatus: 500,
             requestId: context.awsRequestId,
-        }
+        };
         callback(JSON.stringify(myErrorObj));
     }
 };

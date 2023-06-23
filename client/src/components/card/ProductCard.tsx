01a88ef4b53314a94f7f8d2984b66d0d04afe18d
Lorenzo Marcolli <lory.marcolli@gmail.com>
2023-06-23 11:54:07 +0200

    fix: quantity

--- client/src/components/card/ProductCard.tsx
+++ client/src/components/card/ProductCard.tsx
@@ -50,6 +50,7 @@ function ProductCard({name, price, imageUrl, productId, quantity}: Product ) {
       </div>
     </div>
     <button
+    disabled={quantity === 0}
     onClick={() => {
         // add timeout and loading state to button
         setTimeout(() => {
@@ -77,11 +78,11 @@ function ProductCard({name, price, imageUrl, productId, quantity}: Product ) {
         addToCart(productId, name, price, imageUrl)
     }
     }
-    className="flex items-center w-full justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+    className="flex disabled:opacity-50 items-center w-full justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
       <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
       </svg>
-      {loading ? 'Adding to cart...' : 'Add to cart'}
+      {loading ? 'Adding to cart...' : quantity === 0? 'Sold out' : 'Add to cart'}
       </button>
   </div>
 </div>

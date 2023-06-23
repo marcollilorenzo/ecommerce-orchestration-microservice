0df88a9e6efda79faa15a5ec4e1d2604025b1452
Alberto Rizzi <albertorizzi@MBP-di-Alberto.homenet.telecomitalia.it>
2023-06-23 14:31:46 +0200

    feat: add clear cart

--- client/src/app/order/page.tsx
+++ client/src/app/order/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { OrderStatus, WebScoketMessage } from "@/types/generic";
 import { Player } from '@lottiefiles/react-lottie-player';
@@ -8,6 +8,7 @@ import completedAnimation from '../../../src/animation/completed.json'
 import laodingAnimation from '../../../src/animation/loading.json'
 import failedAnimation from '../../../src/animation/failed.json'
 import paymentAnimation from '../../../src/animation/payment.json'
+import { clearCart } from "@/utils/cart";
 
 
 
@@ -17,99 +18,104 @@ function Order() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl");
 
- // state for order status (pending, completed, failed)
-    const [orderStatus, setOrderStatus] = useState<OrderStatus>("pending");
-    const [messageWebsocket, setMessageWebsocket] = useState<string>('We receive the order, waiting...');
+  // state for order status (pending, completed, failed)
+  const [orderStatus, setOrderStatus] = useState<OrderStatus>("pending");
+  const [messageWebsocket, setMessageWebsocket] = useState<string>('We receive the order, waiting...');
 
-    const [websocketStatus, setWebsocketStatus] = useState<"connected" | "disconnected" | "error">('disconnected')
+  const [websocketStatus, setWebsocketStatus] = useState<"connected" | "disconnected" | "error">('disconnected')
 
   if (!callbackUrl) {
     return <div>Invalid callbackUrl</div>;
   }
 
   // check if callbackUrl is valid
-    if (!callbackUrl.startsWith("ws")) {
-        return <div>Invalid callbackUrl</div>;
+  if (!callbackUrl.startsWith("ws")) {
+    return <div>Invalid callbackUrl</div>;
+  }
+
+  //open websocket connection with try catch
+  let ws: WebSocket | null = null;
+  try {
+    ws = new WebSocket(callbackUrl)
+  } catch (error) {
+    console.log(error)
+  }
+
+
+
+  if (!ws) {
+    return <div>Invalid callbackUrl</div>;
+  }
+
+  // connection error
+  ws.onerror = (error) => {
+    console.log(error)
+    setWebsocketStatus('error')
+  }
+
+  // connection opened
+  ws.onopen = () => {
+    console.log('connected')
+    setWebsocketStatus('connected')
+  }
+
+  // listen for messages
+  ws.onmessage = (event) => {
+
+    const data: WebScoketMessage = JSON.parse(event.data)
+    console.log(data)
+
+    if (data.status === "completed") {
+      clearCart()
+    }
+    setOrderStatus(data.status)
+    setMessageWebsocket(data.message)
+  }
+
+  // disconnect websocket connection
+  const disconnect = () => {
+    if (ws) {
+      ws.close()
+      setWebsocketStatus('disconnected')
     }
 
-    //open websocket connection with try catch
-    let ws: WebSocket | null = null;
-    try {
-        ws = new WebSocket(callbackUrl)
-    } catch (error) {
-        console.log(error)
-    }
+  }
 
-
- 
-    if (!ws) {
-        return <div>Invalid callbackUrl</div>;
-    }
-
-    // connection error
-    ws.onerror = (error) => {
-        console.log(error)
-        setWebsocketStatus('error')
-    }
-
-    // connection opened
-    ws.onopen = () => {
-        console.log('connected')
-        setWebsocketStatus('connected')
-    }
-
-    // listen for messages
-    ws.onmessage = (event) => {
-        const data: WebScoketMessage = JSON.parse(event.data)
-        console.log(data)
-        setOrderStatus(data.status)
-        setMessageWebsocket(data.message)
-    }
-
-    // disconnect websocket connection
-    const disconnect = () => {
-        if (ws) {
-            ws.close()
-            setWebsocketStatus('disconnected')
-    }
-
-}
-    
 
   return (
     <section>
       <div className="h-[15vh] bg-yellow-200 rounded-lg relative flex justify-center items-center">
         <h2 className="text-center">Order status</h2>
-        
+
       </div>
 
       {
-            websocketStatus === 'connected' && (
-    
-        <div className="flex flex-col my-8 justify-center items-center">
+        websocketStatus === 'connected' && (
+
+          <div className="flex flex-col my-8 justify-center items-center">
             {
-               
-                    <p>{messageWebsocket}</p>
-              
+
+              <p>{messageWebsocket}</p>
+
             }
             <Player
-                autoplay
-                loop
-                src={
-                    orderStatus === "pending"
-                        ? laodingAnimation
-                        : orderStatus === "completed"
-                        ? completedAnimation
-                        : orderStatus === 'payment'
-                        ? paymentAnimation
-                        : failedAnimation
+              autoplay
+              loop
+              src={
+                orderStatus === "pending"
+                  ? laodingAnimation
+                  : orderStatus === "completed"
+                    ? completedAnimation
+                    : orderStatus === 'payment'
+                      ? paymentAnimation
+                      : failedAnimation
 
-                }
-               className="w-1/2 h-1/2"
+              }
+              className="w-1/2 h-1/2"
             >
             </Player>
-        </div>
-            )}
+          </div>
+        )}
     </section>
   );
 }

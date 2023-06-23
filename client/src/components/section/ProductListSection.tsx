1d447b0db4858d4c00ab804a46390eae177665bc
Alberto Rizzi <albertorizzi@MBP-di-Alberto.homenet.telecomitalia.it>
2023-06-23 14:06:28 +0200

    force reload

--- client/src/components/section/ProductListSection.tsx
+++ client/src/components/section/ProductListSection.tsx
@@ -10,42 +10,41 @@ function ProductListSection() {
     const [loading, setLoading] = useState(false)
     useEffect(() => {
         setLoading(true)
-        fetch('/api/products', {cache: 'no-cache'})
-        .then((res) => res.json())
-        .then((data) => {
+        fetch('/api/products', { cache: 'reload' })
+            .then((res) => res.json())
+            .then((data) => {
 
-            if(data.error) {
-                return
-            }
-            setProducts(data.body)
-            setLoading(false)
-        })
-        .catch((err) => 
-        {
-            setLoading(false)
-        console.log(err)
-    })
+                if (data.error) {
+                    return
+                }
+                setProducts(data.body)
+                setLoading(false)
+            })
+            .catch((err) => {
+                setLoading(false)
+                console.log(err)
+            })
     }, [])
 
 
-  return (
-    <div className='pb-8'>
-        <h2 className='mb-4'>Product List</h2>
-        <section>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {
-                    loading ? <div className='text-left'>Loading...</div> : null
-                }
-                {products.map((product) => (
-                    <ProductCard 
-                    key={product.productId}
-                    {...product}
-                    />
-                ))}
-            </div>
-        </section>
-    </div>
-  )
+    return (
+        <div className='pb-8'>
+            <h2 className='mb-4'>Product List</h2>
+            <section>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {
+                        loading ? <div className='text-left'>Loading...</div> : null
+                    }
+                    {products.map((product) => (
+                        <ProductCard
+                            key={product.productId}
+                            {...product}
+                        />
+                    ))}
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default ProductListSection
